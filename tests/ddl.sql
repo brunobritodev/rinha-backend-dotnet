abfde7fd2b9d9b1ea997fa93663b0f25acedb76d
Bruno Brito <bhdebrito@gmail.com>
2023-09-22 16:53:51 -0300

    fix: ddl

--- tests/ddl.sql
+++ tests/ddl.sql
@@ -1,17 +1,3 @@
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
-SET default_tablespace = '';
-
-SET default_table_access_method = heap;
-
 CREATE UNLOGGED TABLE "Pessoas" (
     "Id" uuid NOT NULL CONSTRAINT "PK_Pessoas" PRIMARY KEY,
     "Apelido" character varying(32) NOT NULL,
